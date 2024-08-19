a8f7853e06e0b896d3426932ec56f6b833d2af52
Ofmszr <ofmszrhesap@gmail.com>
2024-08-19 12:41:40 +0300

    asd

--- assets/java script/10-primite-vs-reference.js
+++ assets/java script/10-primite-vs-reference.js
@@ -2,3 +2,4 @@
 //string number bigint boolean symbol undefined null
 //reference
 // object array 
+// aradaki fark primite de hafıza yok bir değisiklik kalıcı olmuyor ama reference lerde değisiklik kalıcı oluyor
---
+++ assets/java script/11-aritmatic-operators..js
@@ -0,0 +1,21 @@
+//addition
+//Adding number + number
+
+
+const EggPrice = 3 ;
+const CheseePrice = 5 ;
+const MeatPrice = 12 ;
+const TotalPrice = EggPrice + CheseePrice + MeatPrice ;
+console.log("expense: $ " , TotalPrice);
+
+// addding number + string
+
+const x =5;
+const y ="4";
+const z = x + y ;
+console.log(z)
+
+// adding string + string
+const word = "hello";
+const oword = "word";
+console.log(word , oword);
