45dbb393f63dcabccb12bb8cc4f247d00ddc755e
Ofmszr <ofmszrhesap@gmail.com>
2024-08-24 16:08:05 +0300

    asdd

--- assets/java script/22-dom-methods.js
+++ assets/java script/22-dom-methods.js
@@ -1,6 +1,15 @@
 //DOM PROPERTIES AND METHODS
 //Inner text
+//Inner html
 const h2EL = document.querySelector("#article1 h2")
 console.log(h2EL)
 console.log(h2EL.innerText)
-//
+h2EL.innerText ="EYYOOO0<em> OO </em>";
+h2EL.innerHTML ="EYYOOO0<em> OO </em>";
+const pEl =document.querySelector("p")
+console.log(pEl.innerHTML)
+console.log(pEl.innerText)
+const liEl = document.querySelector(".li")
+
+//-------------------------------
+console.log
