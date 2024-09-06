35969ddde56bb192cc4e42d025d728824fb75bea
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-06 19:25:31 +0300

    asd

--- assets/java script/fonk-temel.js
+++ assets/java script/fonk-temel.js
@@ -2,3 +2,14 @@ function fonksiyonAdi(parametreler) {
     // Fonksiyonun gerçekleştireceği işlemler
     return sonuc; // Sonuç döndürebilir
   }
+  function carp(x, y) { // x ve y parametrelerdir
+    return x * y;
+  }
+  
+  console.log(carp(4, 5)); // 4 ve 5 argümanlardır
+  carp(4, 5); // Fonksiyon burada çağrılıyor
+  function fark(a, b) {
+    return a - b; // Sonucu geri döndür
+  }
+  
+  let sonuc = fark(10, 3); // sonuc değişkeni 7 olur
