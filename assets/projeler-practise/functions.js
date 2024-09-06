ef02f3e50bdf9117156ab0f89263683c3889cc9c
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-06 18:52:44 +0300

    asd

--- assets/projeler-practise/functions.js
+++ assets/projeler-practise/functions.js
@@ -1,1 +1,12 @@
-// functions are
+// functions are used to create a new project and delete a project from the list of projects in the database 
+function topla(a, b) {
+    return a + b;
+  }
+  let sonuc = topla(5, 10); // sonuc değişkeni 15 olur.
+console.log(sonuc); // 15 yazdırılır.
+const carp = function(x, y) {
+    return x * y;
+  };
+  console.log(carp(4, 5)); // 20 yazdırılır.
+  const topla = (a, b) => a + b; // Aynı işlev, fakat daha kısa.
+console.log(topla(2, 3)); // 5 yazdırılır.
