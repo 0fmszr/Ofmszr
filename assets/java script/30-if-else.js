2e5aaddc7a2840bda66f098a5f62b71b07a9d196
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-11 22:32:46 +0300

    chore: Refactor DOM manipulation code for better readability and maintainability

---
+++ assets/java script/30-if-else.js
@@ -0,0 +1,19 @@
+// Example code using if-else statements
+
+// Define a variable
+let num = 10;
+
+// Check if the number is greater than 0
+if (num > 0) {
+    console.log("The number is positive");
+} else if (num === 0) {
+    console.log("The number is zero");
+} else {
+    console.log("The number is negative");
+}
+//The if-else statement is used to make decisions in your code based on certain conditions.
+//It consists of an if block, followed by an optional else block.
+//The condition inside the if statement is evaluated. If it is true, the code inside the if block is executed. Otherwise, the code inside the else block is executed.
+//In the example code you provided, the variable num is checked to see if it is greater than 0. If 
+//it is, the message "The number is positive" is logged to the console. Otherwise, the message "The number is not positive" is logged.
+//You can have multiple if-else
