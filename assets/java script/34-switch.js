7e6492840da4d5256ad683e8b2d819d433a7b9ed
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-15 10:55:35 +0300

    feat: Add HTML and JavaScript files for switch statement example

---
+++ assets/java script/34-switch.js
@@ -0,0 +1,18 @@
+console.log("switch is working bro ...")
+
+//switch
+const myFavorite = "choclate" ;
+
+switch(myFavorite){
+    case "valilla":
+        console.log("this is my favorite vanil");
+        break;
+        case "strawberry":
+        console.log("this is my favorite straw");
+        break;
+        case "choclate":
+        console.log("this is my favorite choc");
+        break;
+        default:
+            console.log("i want different flavor bro")
+}
