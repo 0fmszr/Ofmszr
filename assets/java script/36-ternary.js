361b89b15037240b8bbcd1aa4c3bda96823b577c
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-15 11:23:28 +0300

    Add HTML and JavaScript files for ternary operator practice

---
+++ assets/java script/36-ternary.js
@@ -0,0 +1,8 @@
+console.log("36-ternary.js is working ...")
+
+//ternary
+
+let myFavorite = "choclate";
+let sellerCream = "vanilla";
+
+console.log(myFavorite === sellerCream ? "i can get choclate" : "if u dont have choc i can get vanilla man")
