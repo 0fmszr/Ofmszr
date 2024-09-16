d961a3f7318decc3ca07f5f18df38f6283d1266f
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-16 20:19:54 +0300

    Add HTML and JavaScript files for optional chaining practice

---
+++ assets/java script/38-optional-chainig.js
@@ -0,0 +1,9 @@
+console.log("is it working man fuck ??")
+
+const user = {
+    name: "fatih pro" ,
+    age: 22,
+
+}
+//console.log(user.salary.amount); this is a mistage but with optional chainging we can work it out
+console.log(user?.salary?.amount);
