538aa5d8709812ac8f50e13d32216519f61ee985
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-17 11:03:10 +0300

    Add HTML and JavaScript files for while loop practice

---
+++ assets/java script/46-while-loop.js
@@ -0,0 +1,11 @@
+console.log("46-while-loop.js is working ")
+
+for(let index = 0; index<5; index++){
+    console.log("intex count:" + index)
+}
+
+let index2 = 0;
+while(index2< 5 ){
+    console.log(index2)
+    index2++
+}
