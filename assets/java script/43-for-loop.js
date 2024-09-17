2b76fa1bb923cba8a3bc2a672adc6550dadd4e15
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-17 10:14:59 +0300

    Add HTML and JavaScript files for for loop practice

---
+++ assets/java script/43-for-loop.js
@@ -0,0 +1,12 @@
+console.log("43-for-loop.js is working")
+
+
+//for loop 
+let x = 5;
+if(x<5){
+
+}
+// starting point ,condition , inc/dec
+for(let index=12; index<100; index++){
+    console.log(index)
+}
