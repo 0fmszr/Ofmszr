b96e5ea91e421de45fbf4a755fd8e509d7430d0b
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-09-17 09:16:45 +0300

    Add HTML and JavaScript files for OR and AND operators practice

---
+++ assets/java script/39-or-and-operators.js
@@ -0,0 +1,7 @@
+console.log("39-or-and-operators.js is working brow...")
+let isAdmin = true ;
+const keyword = isAdmin && "you are admin wuhu";
+console.log(keyword)
+
+let keyword2 = isAdmin || "user";
+console.log(keyword2)
