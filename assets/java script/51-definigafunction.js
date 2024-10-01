ec53a65382e6bfea6dc87aeaec2c21f816302c24
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-10-01 23:28:40 +0300

    asddaa

---
+++ assets/java script/51-definigafunction.js
@@ -0,0 +1,10 @@
+console.log("51-definigafunction.js is workinggg...")
+
+// defining a function => 3 ways
+
+//1. Function Declaration - Traditional way
+
+
+// 2. Function Expression
+
+// 3. arrow Function
