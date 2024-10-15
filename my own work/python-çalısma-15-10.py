11641a678d6751478db6a0758cb701f95ccebcb5
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-10-15 22:44:42 +0300

    Refactor code to fix ModuleNotFoundError and add Python calculator

---
+++ my own work/python-çalısma-15-10.py
@@ -0,0 +1,18 @@
+# first class
+name = "Ofmszr"
+print(name)
+
+age = 19
+age += 1
+print(age)
+print(type(age))
+print("Your age is : " + str(age))
+
+height = 182.2
+print("your height is : " + str(height) + " cm") 
+print(type(height))
+
+human = False
+print(human)
+print(type(human))
+
---
+++ python-çalısma-15-10-2.py
@@ -0,0 +1,4 @@
+name , age , alive = "ofmszr" , 19 , True
+age_a =age_b = age_c = 19
+print(name)
+print(age_a)
