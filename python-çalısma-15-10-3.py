bd65b696460330bf71abea569d418fb869328afb
Ömer Fatih Mehmet Sizer <ofmszrhesap@gmail.com>
2024-10-15 22:57:38 +0300

    Refactor code to add string manipulation operations in Python

---
+++ python-çalısma-15-10-3.py
@@ -0,0 +1,12 @@
+name = "ofmszr"
+print(len(name))
+print(name.find("o"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("f"))
+print(name.count("o"))
+print(name.replace("o" , "ö"))
+print(name*4)
